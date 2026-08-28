11d3b05548e17b59df193f4726b6683557f45387
Ilson Ricardo <38836155+risoweb@users.noreply.github.com>
2026-08-28 14:22:41 -0300

    Remove base path configuration from vite config
    Remove base configuration for deployment path.

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',  // <-- isso diz que vai ficar em /ir-design/
   plugins: [react()],
 })
