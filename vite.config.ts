9f679d73a5ab21550aa52285d4e5ff913b0bb083
Ilson Ricardo <38836155+risoweb@users.noreply.github.com>
2026-08-28 14:20:42 -0300

    Change base path in Vite configuration
    novo base

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/ir-design/',  // <-- isso diz que vai ficar em /ir-design/
+  base: '/',  // <-- isso diz que vai ficar em /ir-design/
   plugins: [react()],
 })
